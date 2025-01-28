214d25a4bdadec4b445db1ef4450f9d08872c076
MUHAMAD HARITH IQBAL BIN SALLEH <2023385509@student.uitm.edu.my>
2025-01-28 14:05:08 +0800

    add registration page with bcrypt

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,28 +1,35 @@
 "use client";
+import { set } from "mongoose";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function RegisterPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    const [name, setName] = useState("");
+    const [username, setUsername] = useState("");
     const [phone, setPhone] = useState("");
     const [role, setRole] = useState("");
+    const [creatingUser, setCreatingUser] = useState(false);
+    const [userCreated, setUserCreated] = useState(true);
 
-    function handleFromSubmit(ev){
+     async function handleFromSubmit(ev){
         ev.preventDefault();
-        fetch("/api/register", {
+        setCreatingUser(true);
+        await fetch("/api/register", {
             method: "POST",
             body: JSON.stringify({
                 email,
                 password,
-                name,
+                username,
                 phone,
                 role
             }),
             headers: {
                 "Content-Type": "application/json"
             },
-        })
+        });
+        setCreatingUser(false);
+        setUserCreated(true);
 
     }
 
@@ -30,20 +37,27 @@ export default function RegisterPage() {
         <section className="mt-8">
             <h1 className="text-center text-primary text-4xl ">Register</h1>
             <form className="block max-w-sm mx-auto" onSubmit={handleFromSubmit}>
-                <input type="email" placeholder="email" value={email} onChange={ev => setEmail(ev.target.value)} />
-                <input type="password" placeholder="password" value={password} onChange={ev => setPassword(ev.target.value)} />
-                <input type="text" placeholder="name" value={name} onChange={ev => setName(ev.target.value) }/>
-                <input type="text" placeholder="phone number" value={phone} onChange={ev => setPhone(ev.target.value) }/>
+                { userCreated && (
+                    <div className="my-4">
+                        User created successfully.<br />
+                        Please {''} 
+                        <Link className="underline" href={'/login'}>Login &raquo;</Link>.
+                    </div>
+                )}
+                <input type="email" placeholder="email" value={email} disabled={creatingUser} onChange={ev => setEmail(ev.target.value)} />
+                <input type="password" placeholder="password" value={password} disabled={creatingUser} onChange={ev => setPassword(ev.target.value)} />
+                <input type="text" placeholder="username" value={username} disabled={creatingUser} onChange={ev => setUsername(ev.target.value) }/>
+                <input type="text" placeholder="phone number" value={phone} disabled={creatingUser} onChange={ev => setPhone(ev.target.value) }/>
                 
                 <label className="flex flex-col">
                 Pick a role:
-                <select value={role} onChange={ev => setRole(ev.target.value)} defaultValue={"businessOwner"}>
+                <select value={role} disabled={creatingUser} onChange={ev => setRole(ev.target.value)} defaultValue={"businessOwner"}>
                     <option value="businessOwner">Business Owner</option>
                     <option value="waiter">Waiter</option>
                     <option value="kitchenStaff">Kitchen Staff</option>
                 </select>
                 </label>
-                <button type="submit">Register</button>
+                <button disabled={creatingUser} type="submit">Register</button>
             </form>
         </section>
     );
---
+++ src/models/User.js
@@ -0,0 +1,27 @@
+import { Schema } from "mongoose";
+import { model, models } from "mongoose";
+import bcrypt from "bcrypt";
+
+const UserSchema = new Schema({
+    email: {type: String, required: true, unique: true},
+    username:{type: String, required: true},
+    password: {
+        type: String, 
+        required: true, 
+        validate: {
+            validator: pass => pass.length >= 5,
+            message: "Password must be at least 5 characters long"
+        },
+    },
+    phone:{type: String, required: true},
+    role:{type: String, required: true, enum: ["businessOwner", "waiter", "kitchenStaff"]},
+},{timestamps: true}) 
+
+UserSchema.post('validate',function(user){
+    const notHashedPassword = user.password;
+    const salt = bcrypt.genSaltSync(10);
+    user.password = bcrypt.hashSync(notHashedPassword, salt);
+});
+
+
+export const User = models?.User || model("User", UserSchema);
